06d373776ce86f684f8e225687f12c4ac353ac4d
Immacule Kayinda <immakaymw@gmail.com>
2024-10-01 19:24:36 +0100

    basic configuration of profile tsx

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { tweetsType } from "../App";
+import { useParams } from "react-router-dom";
+import TweetList from "./TweetList";
 
-export default function Profile() {
-  return <div>
-    Profile
-  </div>;
+export default function Profile({ tweets }: { tweets: tweetsType[] }) {
+  const { username } = useParams();
+  const userSortedTweets = tweets
+    .filter((tweet) => tweet.username === username)
+    .sort((a, b) => {
+      return (
+        new Date(a.publishedAt).getTime() - new Date(b.publishedAt).getTime()
+      );
+    });
+  return (
+    <div>
+      <h1>Profile de {username}</h1>
+      <TweetList tweets={userSortedTweets} />
+    </div>
+  );
 }
